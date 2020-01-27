be1a125c5ef284ae4e324139ca149e86ab763d16
cristi <crissduna@gmail.com>
2020-01-27 12:15:37 +0200

    Added friends fetch functionality for API

---
+++ son/api/routes/friends.js
@@ -0,0 +1,50 @@
+const express = require('express');
+const router = express.Router();
+const User = require('../../models/user');
+const networks = ['facebook','lastfm','twitter'];
+
+router.get('/:network', async function(req,res) {
+    const network = req.params.network;
+    if(!networks.includes(network)) {
+        res.status(404);
+        return res.json({msg: 'Invalid network'});
+    }
+    try {
+        const foundUser = await User.findById(req.userId);
+        const userNetwork = foundUser.accounts.get(network);
+
+        const networkFriends = foundUser.populate('friends','email',{network},async function(err,userFriendsId){
+            const friendsIdentifier = userFriendsId.friends.map(friend => friend.email);
+            //const filter = `accounts.${network}`;
+            let friends;
+            switch(network) {
+                case 'facebook':
+                    friends = await User.find({'accounts.facebook' : {$in: friendsIdentifier}});
+                    break;
+                case 'lastfm':
+                    friends = await User.find({'accounts.lastfm' : {$in: friendsIdentifier}});
+                    break;
+                case 'twitter':
+                    friends = await User.find({'accounts.twitter' : {$in: friendsIdentifier}});
+                    break;
+                default:
+                    break;
+
+            }
+            friends = friends.map(friend => {
+                return {...friend._doc,password : null}
+            });
+            res.status(200);
+            return res.json({data: friends});
+
+        });
+    }
+    catch(e) {
+        console.log(e);
+        res.status(500);
+        return res.json({msg: e.message});
+    }
+
+});
+
+module.exports = router;
--- son/api/routes/merge.js
+++ son/api/routes/merge.js
@@ -2,9 +2,10 @@ const express = require('express');
 const router = express.Router();
 
 const authRouter = require('./auth');
-
+const friendsRouter = require('./friends');
 
 router.use('/accounts',authRouter);
+router.use('/friends',friendsRouter);
 
 
 module.exports = router;
